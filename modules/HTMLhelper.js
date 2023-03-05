9f8a744bc01bec5226f2b01c54eff8df4514bbda
Ale Barrios <al.barrios88@gmail.com>
2023-03-05 16:11:41 -0300

    Agrego header, main y footer vacios

--- modules/HTMLhelper.js
+++ modules/HTMLhelper.js
@@ -6,13 +6,25 @@ export default class HTMLhelper{
      * Crea un objeto tipo HTMLhelper.
      * @param {Document} _documento - la referencia al DOM.
      */
-        constructor(documento){
-            this.#document = documento;
-            this.crearContenedor({
-                texto: "Hola Viteh",
-                tipo: "div"
-            });
-        }
+    constructor(documento){
+        this.#document = documento;
+        this.#inicializarHTML();
+    }
+
+    #inicializarHTML(){
+        const headerElement = this.#document.createElement("header");
+        headerElement.innerHTML = `<h1>Segunda Pre-Entrega del proyecto final</h1>`;
+
+        const mainElement = this.#document.createElement("main");
+        mainElement.innerHTML = `Este es el main`;
+
+        const footerElement = this.#document.createElement("footer");
+        footerElement.innerHTML = `Este es el footer`; 
+
+        this.#document.body.append(headerElement);
+        this.#document.body.append(mainElement);
+        this.#document.body.append(footerElement);
+    }
 
     crearContenedor({texto,tipo}){
         const elemento = document.createElement(tipo);
